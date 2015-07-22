c3eebdf2b15fb4b92019ba6d146210a9d6e31bc5
Timothy Yudi <timothyyudi@gmail.com>
2015-07-22 18:09:24 +0700

    outer measurement. no cap file

--- src/timothyyudi/ahocorasickmulticharacter/controller/AhoCorasick.java
+++ src/timothyyudi/ahocorasickmulticharacter/controller/AhoCorasick.java
@@ -170,7 +170,7 @@ public class AhoCorasick {
 		int inputStringLength = inputString.length();
 		int inputStringLastPosition = inputStringLength -1;
 		
-//		algoStart=System.nanoTime();
+		algoStart=System.nanoTime();
 		for (int i = 0; i < inputStringLength; i++) { //as long as there is an input
 			
 			columnNumberCounter++;
@@ -185,7 +185,7 @@ public class AhoCorasick {
 			
 			if(inputStringBuffer.length()==2 || i==inputStringLastPosition){
 				
-				algoStart=System.nanoTime();
+//				algoStart=System.nanoTime();
 				
 				bufferStr0 = ""+inputStringBuffer.charAt(0);
 				
@@ -221,17 +221,17 @@ public class AhoCorasick {
 				//do some xploration and search
 				inputStringBuffer = ""; //reset buffer
 				
-				algoEnd=System.nanoTime();
-				ahoCorasickTimeFragment=algoEnd-algoStart;
-				ahoCorasickTimeTotal+=ahoCorasickTimeFragment;
+//				algoEnd=System.nanoTime();
+//				ahoCorasickTimeFragment=algoEnd-algoStart;
+//				ahoCorasickTimeTotal+=ahoCorasickTimeFragment;
 
 			}
 
 		}
-//		algoEnd=System.nanoTime();
+		algoEnd=System.nanoTime();
 		Utility util = new Utility();
-		util.writeAhoCorasickTime(ahoCorasickTimeTotal);
-//		util.writeAhoCorasickTime(algoEnd-algoStart);
+//		util.writeAhoCorasickTime(ahoCorasickTimeTotal);
+		util.writeAhoCorasickTime(algoEnd-algoStart);
 	}
 	
 	/**prepare output for the matching keywords found*/
